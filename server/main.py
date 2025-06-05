9265029092124c2c7c55ca0b0ab6c743c0bb13f1
Your Name <biswastatay73@gmail.com>
2025-06-05 13:41:52 +0530

    Implement SortSourceService for sorting search results based on similarity

--- server/main.py
+++ server/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
-
 from pydantic_models.chat_body import ChatBody
 from services.search_service import SearchService
+from services.sort_source_service import SortSourceService
 
 
 app = FastAPI()
@@ -12,6 +12,7 @@ app = FastAPI()
 
 
 search_service = SearchService()
+sort_source_service = SortSourceService()
 
 
 #chat
@@ -19,4 +20,5 @@ search_service = SearchService()
 def chat_endpoint(body: ChatBody):
     search_results = search_service.web_search(body.query)
     print(search_results)
+    sort_source_service.sort_sources(body.query, search_results)
     return body.query
